d1a2dbf4a3e2d6c1988d395f1fbc88f33877c14b
Juan Carlos Bindez <79322362+JuanBindez@users.noreply.github.com>
2022-02-18 15:11:21 -0300

    Update teste.js

--- js/teste.js
+++ js/teste.js
@@ -1,6 +1,6 @@
 const botao = document.getElementById("btnClique")
 
-botao.addEventListener("click", janela)
+botao.addEventListener("click", escolha)
 
 function janela(){
     window.alert('esta janela')
@@ -10,3 +10,50 @@ function janela(){
 }
 
 
+function adicao(){
+    var numero1 = Number.parseFloat(window.prompt('digite um numero'))
+    var numero2 = Number.parseFloat(window.prompt('digite outro numero'))
+    var r = numero1 + numero2
+    window.alert('o resultado é:' + "" + r)
+}
+
+
+function mult(){
+    var numero1 = Number.parseFloat(window.prompt('digite um numero'))
+    var numero2 = Number.parseFloat(window.prompt('digite outro numero'))
+    var r = numero1 * numero2
+    window.alert('o resultado é:' + "" + r)
+}
+
+
+function sub(){
+    var numero1 = Number.parseFloat(window.prompt('digite um numero'))
+    var numero2 = Number.parseFloat(window.prompt('digite outro numero'))
+    var r = numero1 - numero2
+    window.alert('o resultado é:' + "" + r)
+}
+
+
+function divi(){
+    var numero1 = Number.parseFloat(window.prompt('digite um numero'))
+    var numero2 = Number.parseFloat(window.prompt('digite outro numero'))
+    var r = numero1 / numero2
+    window.alert('o resultado é:' + "" + r)
+}
+
+
+function escolha(){
+    var operador = window.prompt('digite o operador:')
+    if (operador == "a"){
+        adicao()
+    }
+    if (operador == "m"){
+        mult()
+    }
+    if (operador == "s"){
+        sub()
+    }
+    if (operador == "d"){
+        divi()
+    }
+}
